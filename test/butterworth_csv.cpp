2d9f0e63aa8542b31fac24076a49f5c26f8572f4
Aleix Paris <aleix@mit.edu>
2019-11-11 16:50:39 -0500

    update

--- test/butterworth_csv.cpp
+++ test/butterworth_csv.cpp
@@ -4,32 +4,39 @@
 
 #include "assert_print.h"
 
+#include <iostream>
+#include <fstream>
+
 int main (int,char**)
 {
-        // create the filter structure for 5th order
-        Iir::Butterworth::LowPass<5> f;
 
-	// filter parameters
-        const float samplingrate = 100; // Hz
-        const float cutoff_frequency = 2; // Hz
+    // Open the csv input file
+    //
 
-	// calc the coefficients
-	f.setup (samplingrate, cutoff_frequency);
-	
-	double b = 0;
-	double b2 = 0;
-	for(int i=0;i<1000000;i++) 
-	{
-		float a=0;
-		if (i==10) a = 1;
-		b2 = b;
-		b = f.filter(a);
-		assert_print(!isnan(b),"Lowpass output is NAN\n");
-		if ((i>20) && (i<100))
-			assert_print((b != 0) || (b2 != 0),
-				     "Lowpass output is zero\n");
-	}
-	assert_print(fabs(b) < 1E-25,"Lowpass value for t->inf to high!");
+    // Open the csv output file
+    std::ofstream myfile;
+    myfile.open ("filtered.csv");
+    myfile << "angles_x,angles_x_lowpass" << std::endl;
 
-	return 0;
+
+    // create the filter structure for 5th order
+    Iir::Butterworth::LowPass<5> f;
+
+    // filter parameters
+    const float samplingrate = 100; // Hz
+    const float cutoff_frequency = 2; // Hz
+
+    // calc the coefficients
+    f.setup (samplingrate, cutoff_frequency);
+
+    double b = 0;
+    for(int i=0;i<..;i++)
+    {
+        b = f.filter(a);
+        assert_print(!isnan(b),"Lowpass output is NAN\n");
+        myfile << a << "," << b << std::endl;
+    }
+
+    myfile.close();
+    return 0;
 }
